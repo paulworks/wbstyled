eab4d87f345280f7974d48b526b748be2bd74f53
paul saary <paul-saary@web.de>
2012-03-30 18:33:05 -0500

    modified Startpage JS and CSS

--- jquery/jquery-plugins.js
+++ jquery/jquery-plugins.js
@@ -1,3 +1,13 @@
+/**
+* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
+* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
+* 
+* @param  f  onMouseOver function || An object with configuration options
+* @param  g  onMouseOut function  || Nothing (use configuration options object)
+* @author    Brian Cherne brian(at)cherne(dot)net
+*/
+(function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev])}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev])};var handleHover=function(e){var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t)}if(e.type=="mouseenter"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob)},cfg.timeout)}}};return this.bind('mouseenter',handleHover).bind('mouseleave',handleHover)}})(jQuery);
+
 function include_file(filename, filetype) {
 
 	if(!filetype)
@@ -45,7 +55,19 @@ $(document).ready(function()
 	$('form').attr('autocomplete', 'off');
 
 
-
+   //startpage
+   var starttime = 500;
+   $('.textlayer').css('bottom', "-70px");
+   $(".actionblock").hoverIntent(
+   function(){
+    $('.textlayer', this).animate({"bottom": "0px"}  ,starttime);
+    $('.actionblocklink.colored ', this).fadeOut(starttime)
+    
+    },
+    function(){
+    $('.textlayer', this).animate({"bottom": "-70px"},starttime);
+    $('.actionblocklink.colored ', this).fadeIn(starttime)
+   });
 
   //page add
   if($('#addpages').length > 0){
@@ -67,5 +89,7 @@ $(document).ready(function()
   
   //options Tabs
   $("#tabs").tabs({ cookie: { expires: 30 } });
+
+
 
 });
